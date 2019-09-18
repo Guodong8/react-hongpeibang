c152e37d45d3efe66286cbc6f9b8d1ee2ab08e40
guodongdong <g17600173200@163.com>
2019-09-18 17:19:47 +0800

    修改login

--- manage/src/views/user/Login.js
+++ manage/src/views/user/Login.js
@@ -8,15 +8,18 @@ class Login extends React.Component{
     goLoginPhone(){
         this.props.history.push("/loginPhone")
     }
+    goBack(){
+        this.props.history.push("/")
+    }
     render(){
         return(
             <div className="login">
                 <div className="login-header">
-                    <span className="iconfont icon-icon_left"></span>
+                    <span className="iconfont icon-icon_left" onClick={this.goBack.bind(this)}></span>
                     <b>登录</b>
                     <span></span>
                 </div>
-                <div className="nav">
+                <div className="login-nav">
                     <img src="https://image.hongbeibang.com/FoBGm71FF5SYiTw0Wc0xkeiL3eQz?imageMogr2/strip/thumbnail/510x510"></img>
                     <button>使用微信登录</button>
                     <p onClick={this.goLoginPhone.bind(this)}>其他方式登录</p>
