c07e426146d95aa3aed31b09a0f2f63d2189a8bb
jiawenqi1552 <382116386@qq.com>
2019-09-21 14:55:31 +0800

    skill+首页+调整

--- manage/src/views/Study.js
+++ manage/src/views/Study.js
@@ -38,36 +38,36 @@ class Study extends React.Component{
         <div className="nav">
        
              
-        <div >
+        <div>
              
          
             
        
             
-       <ul >
-       <li >
-           <i className=""></i>
-           <img src="https://image.hongbeibang.com/Fp0nBR7-xD2caoLENB7qZgmu9vZT?80X80&imageView2/1/w/80/h/80"App-link></img>
+       <ul>
+       <li>
+           <i className="">
+           <img src="https://image.hongbeibang.com/Fp0nBR7-xD2caoLENB7qZgmu9vZT?80X80&imageView2/1/w/80/h/80"></img></i>
            <span>技巧百科</span>
            </li>
            
            <li >
            <i className="">
-           <img src="https://image.hongbeibang.com/Fl493FjRZluXqCNJnvhRYw_IGKZO?80X80&imageView2/1/w/80/h/80"></img>
-           </i>
-           
+           <img src="https://image.hongbeibang.com/Fl493FjRZluXqCNJnvhRYw_IGKZO?80X80&imageView2/1/w/80/h/80"></img></i>
+          
            <span>视频学堂</span>
            </li>
+
            <li>
            <i className=""> <img src="https://image.hongbeibang.com/FkU-5lDUu7y7CurjkdJYWh-ZIg3x?80X80&imageView2/1/w/80/h/80"></img></i>
-           
            <span>新手课程</span>
            </li>
+
            <li>
            <i className=""><img src="https://image.hongbeibang.com/FkcHmIr5rX7zXeHET_Rew64lVrgx?80X80&imageView2/1/w/80/h/80"></img></i>
-           
            <span>食品分类</span>
            </li>
+
        </ul>
      
            
@@ -89,15 +89,7 @@ class Study extends React.Component{
     
       )
   }
-  async componentDidMount(){
-        
-    const {data} = await axios.get(`/hpb/index/get?`);
-    console.log(data);
-    this.setState({
-        category:data.data.category[0].item
-    })
-    console.log(this.state.category)
-}
+ 
 
 
 
