f136c4c1989b9a925c9843a44afa95c6e167deca
LiWenTongB <1033492061@qq.com>
2019-09-21 11:07:05 +0800

    修改APP.js加了一个路由及问题模块内的部分内容

--- manage/src/App.js
+++ manage/src/App.js
@@ -6,12 +6,16 @@ import LoginPhone from "./views/user/LoginPhone"
 import Account from "./views/user/Account"
 import Study from "./views/Study"
 import Search from "./views/user/Search"
+import quzi from "./views/ask/twrouter/Quiz"
+import nextstep from "./views/ask/twrouter/nextstep"
+import Detailedquestions from "./views/ask/zxwt/Detailedquestions"
 import {
   BrowserRouter as Router,
   Route,
   Switch
 } from "react-router-dom"
 import Daren from './views/circle/Daren';
+
 
 function App() {
   return (
@@ -24,7 +28,9 @@ function App() {
                 <Route path={"/account"} component={Account}></Route>
                 <Route path={"/study"} component={Study}></Route>
                 <Route path={"/search"} component={Search}></Route>
-
+                <Route path={"/quzi"} component={quzi}></Route>
+                <Route path={"/nextstep"} component={nextstep}></Route>
+                <Route  path={"/detailedquestions/:id/:a"} component={Detailedquestions}></Route>
                 <Route path={"/"} component={Home}></Route>             
           </Switch>
           
--- manage/src/views/ask/zxwt/xwtlist.js
+++ manage/src/views/ask/zxwt/xwtlist.js
@@ -1,6 +1,14 @@
 import  React from  "react"
 import "../../../asset/css/xwtlist.css"
+// import Detailedquestions  from "./Detailedquestions"
 import  axios  from  "axios"
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  NavLink
+} from "react-router-dom"
 export default class Xwtlist extends React.Component{
 
   constructor(){
@@ -14,11 +22,15 @@ export default class Xwtlist extends React.Component{
     const {xxlist}=this.state;
     return(
       <div className="wrap">
+            
         {
           xxlist.map(v=>(
-            <div className="wtlist" key={v.contentQuestionId} >
-            <div className="listtitie">{v.coverTitle}</div>
-            <div className="xiangxi" >
+            <div className="wtlist" key={v.contentQuestionId}>
+             
+             <NavLink className={"listtitie"} to={"/detailedquestions/" +v.contentId+"/"+v.clientId}>{v.coverTitle}</NavLink>
+            
+
+            <div className="xiangxi" key={v.recipe.clientId} >
               
            <img src={v.recipe.image}></img>
             <div className="goodsname">
@@ -26,8 +38,12 @@ export default class Xwtlist extends React.Component{
               <p>作者:{v.recipe.clientName}</p>
             </div>
             </div>
+           
             <div className="answer">
-              <span>暂无回答</span>
+              {
+                v.answerNum>0?<span>{v.answerNum}个回答</span>:<a className="zwhd">暂无回答</a>
+              }
+             
               <span><img src="https://image.hongbeibang.com/FlSZI5KwZLrR9-QXD9Vu7u0lVvCE?48X48&imageView2/1/w/40/h/40"></img>写答案</span>
   
             </div>
@@ -40,11 +56,11 @@ export default class Xwtlist extends React.Component{
     )
   }
   async componentDidMount(){
-    const {data}=await axios.get(`/hongbeibang/question/getNew?_t=1568776893053&csrfToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOjAsImV4cCI6MTc1NzkzNTMzOCwiaWF0IjoxNTY4NTQ2NTM4fQ.bvDr9GLJ1HfbxJSyYBXAvHgc2q-rPDEMuHZ_FgwYbqo&pageIndex=0&pageSize=10`)
-        console.log(data)
+    const {data}=await axios.get(`/hpb/question/getNew?_t=1568776893053&csrfToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOjAsImV4cCI6MTc1NzkzNTMzOCwiaWF0IjoxNTY4NTQ2NTM4fQ.bvDr9GLJ1HfbxJSyYBXAvHgc2q-rPDEMuHZ_FgwYbqo&pageIndex=0&pageSize=10`)
+        // console.log(data)
         this.setState({
           xxlist:data.data.content.data
         })
-        console.log(this.state.xxlist)
+        // console.log(this.state.xxlist)
   }
 }
